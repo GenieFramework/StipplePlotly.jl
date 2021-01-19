a2c72e21c3bb5ed471f26a0c3ac1438d8d2852f6
Adrian <e@essenciary.com>
2021-01-19 10:57:19 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/StipplePlotly.jl
@@ -0,0 +1,5 @@
+module StipplePlotly
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StipplePlotly
+using Test
+
+@testset "StipplePlotly.jl" begin
+    # Write your tests here.
+end
