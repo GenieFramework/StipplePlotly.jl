791ec6060171e521989cb6116f8504770bbdf516
Adrian Salceanu <essenciary@users.noreply.github.com>
2021-02-07 22:27:22 +0100

    Prototype

--- src/Charts.jl
+++ src/Charts.jl
@@ -5,7 +5,7 @@ import Genie.Renderer.Html: HTMLString, normal_element
 
 using Stipple
 
-export PlotLayout, PlotData, plot
+export PlotLayout, PlotData, Trace, plot
 
 const DEFAULT_WRAPPER = Genie.Renderer.Html.template
 
@@ -19,6 +19,23 @@ const PLOT_TYPE_IMAGE = "image"
 const PLOT_TYPE_CONTOUR = "contour"
 const PLOT_TYPE_TABLE = "table"
 const PLOT_TYPE_BOX = "box"
+const PLOT_TYPE_VIOLIN = "violin"
+const PLOT_TYPE_HISTOGRAM = "histogram"
+const PLOT_TYPE_HISTOGRAM2D = "histogram2d"
+const PLOT_TYPE_HISTOGRAM2DCONTOUR = "histogram2dcontour"
+const PLOT_TYPE_OHLC = "ohlc"
+const PLOT_TYPE_CANDLESTICK = "candlestick"
+const PLOT_TYPE_WATERFALL = "waterfall"
+const PLOT_TYPE_FUNNEL = "funnel"
+const PLOT_TYPE_FUNNELAREA = "funnelarea"
+const PLOT_TYPE_INDICATOR = "indicator"
+const PLOT_TYPE_SCATTER3D = "scatter3d"
+const PLOT_TYPE_SURFACE = "surface"
+const PLOT_TYPE_MESH3D = "mesh3d"
+const PLOT_TYPE_CONE = "cone"
+const PLOT_TYPE_STREAMTUBE = "streamtube"
+const PLOT_TYPE_VOLUME = "volume"
+const PLOT_TYPE_ISOSURFACE = "isosurface"
 
 const LAYOUT_TITLE_REF_CONTAINER = "container"
 const LAYOUT_TITLE_REF_PAPER = "paper"
@@ -186,23 +203,47 @@ end
 Base.@kwdef mutable struct PlotData
   plot::String = PLOT_TYPE_SCATTER
 
-  alignmentgroup::String = ""
+  align::Union{String,Nothing} = nothing
+  alignmentgroup::Union{String,Nothing} = nothing
+  alphahull::Union{Int,Float64,Nothing} = nothing
+  anchor::Union{String,Nothing} = nothing
+  aspectratio::Union{Float64,Int,Nothing} = nothing
+  autobinx::Union{Bool,Nothing} = nothing
+  autobiny::Union{Bool,Nothing} = nothing
   autocolorscale::Union{Bool,Nothing} = nothing
   autocontour::Union{Bool,Nothing} = nothing
   automargin::Union{Bool,Nothing} = nothing
-  base::Union{Number,String,Nothing} = nothing
+  bandwidth::Union{Float64,Int,Nothing} = nothing
+  base::Union{Float64,Int,String,Nothing} = nothing
+  baseratio::Union{Float64,Int,Nothing} = nothing
+  bingroup::Union{String,Nothing} = nothing
+  box::Union{Dict,Nothing} = nothing
   boxmean::Union{Bool,String,Nothing} = nothing
+  boxpoints::Union{Bool,String,Nothing} = nothing
+  caps::Union{Dict,Nothing} = nothing
+  cauto::Union{Bool,Nothing} = nothing
   cells::Union{Dict,Nothing} = nothing
-  cliponaxis::Bool = true
+  cliponaxis::Union{Bool,Nothing} = nothing
+  close::Union{Vector,Nothing} = nothing
+  cmax::Union{Float64,Int,Nothing} = nothing
+  cmid::Union{Float64,Int,Nothing} = nothing
+  cmin::Union{Float64,Int,Nothing} = nothing
+  color::Union{String,Nothing} = nothing
   coloraxis::Union{String,Nothing} = nothing
   colorbar::Union{Dict,Nothing} = nothing
   colorscale::Union{Vector,Nothing} = nothing
   columnorder::Union{Vector,Nothing} = nothing
-  columnwidth::Union{Number,Vector{Number},Nothing} = nothing
+  columnwidth::Union{Float64,Int,Vector,Nothing} = nothing
   connectgaps::Union{Bool,Nothing} = nothing
+  connector::Union{Dict,Nothing} = nothing
   constraintext::Union{String,Nothing} = nothing
+  contour::Union{Dict,Nothing} = nothing
   contours::Union{Dict,Nothing} = nothing
+  cumulative::Union{Dict,Nothing} = nothing
   customdata::Union{Vector,Nothing} = nothing
+  decreasing::Union{Dict,Nothing} = nothing
+  delaunayaxis::Union{Char,String,Nothing} = nothing
+  delta::Union{Dict,Nothing} = nothing
   direction::Union{String,Nothing} = nothing
   dlabel::Union{Int,Nothing} = nothing
   domain::Union{Dict,Nothing} = nothing
@@ -210,10 +251,18 @@ Base.@kwdef mutable struct PlotData
   dy::Union{Int,Nothing} = nothing
   error_x::Union{Dict,Nothing} = nothing
   error_y::Union{Dict,Nothing} = nothing
+  error_z::Union{Dict,Nothing} = nothing
+  facecolor::Union{Vector,Nothing} = nothing
   fill::Union{String,Nothing} = nothing
   fillcolor::Union{String,Nothing} = nothing
+  flatshading::Union{Bool,Nothing} = nothing
+  gauge::Union{Dict,Nothing} = nothing
   groupnorm::Union{String,Nothing} = nothing
   header::Union{Dict,Nothing} = nothing
+  hidesurface::Union{Bool,Nothing} = nothing
+  high::Union{Dict,Nothing} = nothing
+  histfunc::Union{String,Nothing} = nothing
+  histnorm::Union{String,Nothing} = nothing
   hole::Union{Float64,Nothing} = nothing
   hovertext::Union{Vector{String},String,Nothing} = nothing
   hoverinfo::Union{String,Nothing} = nothing
@@ -221,74 +270,135 @@ Base.@kwdef mutable struct PlotData
   hoveron::Union{String,Nothing} = nothing
   hoverongaps::Union{Bool,Nothing} = nothing
   hovertemplate::Union{Vector{String},String,Nothing} = nothing
+  i::Union{Vector,Nothing} = nothing
+  intensity::Union{Vector,Nothing} = nothing
+  intensitymode::Union{String,Nothing} = nothing
+  j::Union{Vector,Nothing} = nothing
+  k::Union{Vector,Nothing} = nothing
   ids::Union{Vector{String},Nothing} = nothing
+  increasing::Union{Dict,Nothing} = nothing
   insidetextanchor::Union{String,Nothing} = nothing
   insidetextfont::Union{Font,Nothing} = nothing
   insidetextorientation::Union{String,Nothing} = nothing
+  isomax::Union{Float64,Int,Nothing} = nothing
+  isomin::Union{Float64,Int,Nothing} = nothing
+  jitter::Union{Float64,Nothing} = nothing
   labels::Union{Vector,Nothing} = nothing
   label0::Union{Int,Nothing} = nothing
-  legendgroup::String = ""
+  legendgroup::Union{String,Nothing} = nothing
+  lighting::Union{Dict,Nothing} = nothing
+  lightposition::Union{Dict,Nothing} = nothing
   line::Union{Dict,Nothing} = nothing
+  low::Union{Vector,Nothing} = nothing
+  lowerfence::Union{Vector,Nothing} = nothing
   marker::Union{Dict,Nothing} = nothing
-  meta::Union{Number,String,Nothing} = nothing
+  maxdisplayed::Union{Int,Nothing} = nothing
+  mean::Union{Vector,Nothing} = nothing
+  measure::Union{Vector,Nothing} = nothing
+  meanline::Union{Dict,Nothing} = nothing
+  median::Union{Vector,Nothing} = nothing
+  meta::Union{Float64,Int,String,Nothing} = nothing
   mode::Union{String,Nothing} = nothing
   name::Union{String,Nothing} = nothing
+  nbinsx::Union{Int,Nothing} = nothing
+  nbinsy::Union{Int,Nothing} = nothing
   ncontours::Union{Int,Nothing} = nothing
-  offset::Union{Number,Vector{Number},Nothing} = nothing
-  offsetgroup::String = ""
-  opacity::Float64 = 1.0
+  notched::Union{Bool,Nothing} = nothing
+  notchwidth::Union{Float64,Nothing} = nothing
+  notchspan::Union{Vector,Nothing} = nothing
+  number::Union{Dict,Nothing} = nothing
+  offset::Union{Float64,Int,Vector,Nothing} = nothing
+  offsetgroup::Union{String,Nothing} = nothing
+  opacity::Union{Float64,Nothing} = nothing
+  opacityscale::Union{Float64,Int,Vector,String,Nothing} = nothing
+  open::Union{Vector,Nothing} = nothing
   orientation::Union{String,Nothing} = nothing
   outsidetextfont::Union{Font,Nothing} = nothing
-  pull::Union{Float64,Vector{Float64},Nothing} = nothing
+  points::Union{Bool,String,Nothing} = nothing
+  pointpos::Union{Float64,Nothing} = nothing
+  projection::Union{Dict,Nothing} = nothing
+  pull::Union{Float64,Vector,Nothing} = nothing
+  q1::Union{Vector,Nothing} = nothing
+  q3::Union{Vector,Nothing} = nothing
+  quartilemethod::Union{String,Nothing} = nothing
   reversescale::Union{Bool,Nothing} = nothing
   rotation::Union{Int,Nothing} = nothing
   scalegroup::Union{String,Nothing} = nothing
+  scalemode::Union{String,Nothing} = nothing
+  scene::Union{String,Nothing} = nothing
+  sd::Union{Vector,Nothing} = nothing
   selected::Union{Dict,Nothing} = nothing
-  selectedpoints::Union{Number,String,Nothing} = nothing
-  showlegend::Bool = true
+  selectedpoints::Union{Float64,Int,String,Nothing} = nothing
+  showlegend::Union{Bool,Nothing} = nothing
   showscale::Union{Bool,Nothing} = nothing
+  side::Union{String,Nothing} = nothing
+  sizemode::Union{String,Nothing} = nothing
+  sizeref::Union{Float64,Int,Nothing} = nothing
+  slices::Union{Dict,Nothing} = nothing
   sort::Union{Bool,Nothing} = nothing
   source::Union{String,Nothing} = nothing
+  spaceframe::Union{Dict,Nothing} = nothing
+  span::Union{Vector,Nothing} = nothing
+  spanmode::Union{String,Nothing} = nothing
   stackgaps::Union{String,Nothing} = nothing
   stackgroup::Union{String,Nothing} = nothing
+  starts::Union{Dict,Nothing} = nothing
+  surface::Union{Dict,Nothing} = nothing
+  surfaceaxis::Union{Int,String,Nothing} = nothing
+  surfacecolor::Union{String,Nothing} = nothing
   text::Union{Vector{String},String,Nothing} = nothing
   textangle::Union{String,Nothing} = nothing
   textfont::Union{Font,Nothing} = nothing
   textinfo::Union{String,Nothing} = nothing
   textposition::Union{String,Nothing} = nothing
   texttemplate::Union{Vector{String},String,Nothing} = nothing
+  tickwidth::Union{Float64,Nothing} = nothing
+  totals::Union{Dict,Nothing} = nothing
   transpose::Union{Bool,Nothing} = nothing
-  uirevision::Union{Number,String,Nothing} = nothing
+  u::Union{Vector,Nothing} = nothing
+  uirevision::Union{Float64,Int,String,Nothing} = nothing
   unselected::Union{Dict,Nothing} = nothing
+  upperfence::Union{Vector,Nothing} = nothing
+  v::Union{Vector,Nothing} = nothing
   values::Union{Vector,Nothing} = nothing
+  vertexcolor::Union{Vector,Nothing} = nothing
   visible::Union{String,Bool,Nothing} = nothing
+  w::Union{Vector,Nothing} = nothing
+  whiskerwidth::Union{Float64,Nothing} = nothing
   width::Union{Int,Vector{Int},Nothing} = nothing
   x::Union{Vector,Nothing} = nothing
   x0::Union{Int,String,Nothing} = nothing
   xaxis::Union{String,Nothing} = nothing
-  xcalendar::String = "gregorian"
+  xbingroup::Union{String,Nothing} = nothing
+  xbins::Union{Dict,Nothing} = nothing
+  xcalendar::Union{String,Nothing} = nothing
   xgap::Union{Int,Nothing} = nothing
-  xperiod::Union{Number,String,Nothing} = nothing
+  xperiod::Union{Float64,Int,String,Nothing} = nothing
   xperiodalignment::Union{String,Nothing} = nothing
-  xperiod0::Union{Number,String,Nothing} = nothing
+  xperiod0::Union{Float64,Int,String,Nothing} = nothing
   xtype::Union{String,Nothing} = nothing
   y::Union{Vector,Nothing} = nothing
   y0::Union{Int,String,Nothing} = nothing
   yaxis::Union{String,Nothing} = nothing
-  ycalendar::String = "gregorian"
+  ybingroup::Union{String,Nothing} = nothing
+  ybins::Union{Dict,Nothing} = nothing
+  ycalendar::Union{String,Nothing} = nothing
   ygap::Union{Int,Nothing} = nothing
-  yperiod::Union{Number,String,Nothing} = nothing
+  yperiod::Union{Float64,Int,String,Nothing} = nothing
   yperiodalignment::Union{String,Nothing} = nothing
-  yperiod0::Union{Number,String,Nothing} = nothing
+  yperiod0::Union{Float64,Int,String,Nothing} = nothing
   ytype::Union{String,Nothing} = nothing
   z::Union{Vector,Nothing} = nothing
   zauto::Union{Bool,Nothing} = nothing
+  zcalendar::Union{String,Nothing} = nothing
   zhoverformat::Union{String,Nothing} = nothing
   zmax::Union{Int,Nothing} = nothing
   zmid::Union{Int,Nothing} = nothing
   zmin::Union{Int,Nothing} = nothing
   zsmooth::Union{String,Nothing} = nothing
 end
+
+const Trace = PlotData
 
 function plot(fieldname::Symbol;
               layout::Union{Symbol,PlotLayout} = PlotLayout(),
@@ -303,17 +413,23 @@ function plot(fieldname::Symbol;
   end
 end
 
+function Base.show(io::IO, pd::PlotData)
+  output = "$(pd.plot): \n"
+  for f in fieldnames(typeof(pd))
+    prop = getproperty(pd, f)
+    if prop !== nothing
+      output *= "$f = $prop \n"
+    end
+  end
+
+  print(output)
+end
+
 #===#
 
 function Base.Dict(pd::PlotData)
-  trace = Dict(
-    :type => pd.plot,
-    :showlegend => pd.showlegend,
-    :legendgroup => pd.legendgroup,
-    :opacity => pd.opacity,
-    :cliponaxis => pd.cliponaxis,
-    :alignmentgroup => pd.alignmentgroup,
-    :offsetgroup => pd.offsetgroup,
+  trace = Dict{Symbol,Any}(
+    :type => pd.plot
   )
 
   if pd.textfont !== nothing
@@ -340,26 +456,40 @@ function Base.Dict(pd::PlotData)
     )
   end
 
-  optionals!(trace, pd, [:autocolorscale, :autocontour, :automargin,
-                        :base, :boxmean,
-                        :cells, :coloraxis, :colorbar, :colorscale, :columnorder, :columnwidth,
-                        :connectgaps, :constraintext, :contours, :customdata,
-                        :direction, :dlabel, :domain, :dx, :dy,
-                        :error_x, :error_y,
-                        :fill, :fillcolor,
-                        :groupnorm, :header,
+  optionals!(trace, pd, [:align, :alignmentgroup, :alphahull, :anchor, :aspectratio, :autobinx, :autobiny,
+                        :autocolorscale, :autocontour, :automargin,
+                        :bandwidth, :base, :baseratio, :bingroup, :box, :boxmean, :boxpoints,
+                        :cauto, :cells, :cliponaxis, :close, :color, :cmax, :cmid, :cmin,
+                        :coloraxis, :colorbar, :colorscale, :columnorder, :columnwidth,
+                        :connectgaps, :connector, :constraintext, :contour, :contours, :cumulative, :customdata,
+                        :decreasing, :delta, :delaunayaxis, :direction, :dlabel, :domain, :dx, :dy,
+                        :error_x, :error_y, :error_z,
+                        :facecolor, :fill, :fillcolor, :flatshading,
+                        :gauge, :groupnorm,
+                        :header, :hidesurface, :high, :histfunc, :histnorm,
                         :hole, :hovertext, :hoverinfo, :hovertemplate, :hoverlabel, :hoveron, :hoverongaps,
-                        :ids, :insidetextanchor, :insidetextorientation,
-                        :labels, :label0, :line, :marker, :meta,
-                        :mode, :name, :ncontours, :offset, :orientation, :pull, :reversescale, :rotation,
-                        :scalegroup, :selected, :selectedpoints, :showscale, :sort, :source, :stackgaps, :stackgroup,
+                        :i, :intensity, :intensitymode, :ids, :increasing, :insidetextanchor, :insidetextorientation, :isomax, :isomin,
+                        :j, :jitter, :k,
+                        :labels, :label0, :legendgroup, :lighting, :lightposition, :line, :low, :lowerfence,
+                        :marker, :maxdisplayed, :meanline, :measure, :median, :meta,
+                        :mode,
+                        :name, :nbinsx, :nbinsy, :ncontours, :notched, :notchwidth, :notchspan, :number,
+                        :offset, :offsetgroup, :opacity, :opacityscale, :open, :orientation,
+                        :points, :pointpos, :projection, :pull,
+                        :q1, :q3, :quartilemethod,
+                        :reversescale, :rotation,
+                        :scalegroup, :scalemode, :scene, :sd,
+                        :selected, :selectedpoints, :showlegend,
+                        :showscale, :side, :sizemode, :sizeref, :slices, :sort, :source, :spaceframe, :span, :spanmode,
+                        :stackgaps, :stackgroup, :starts, :surface, :surfaceaxis, :surfacecolor,
                         :text, :textangle, :textinfo,
-                        :textposition, :texttemplate, :transpose,
-                        :uirevision, :unselected, :values, :visible,
-                        :width,
-                        :x, :x0, :xaxis, :xcalendar, :xgap, :xperiod, :xperiodalignment, :xperiod0, :xtype,
-                        :y, :y0, :yaxis, :ycalendar, :ygap, :yperiod, :yperiodalignment, :yperiod0, :ytype,
-                        :z, :zauto, :zhoverformat, :zmax, :zmid, :zmin, :zsmooth])
+                        :textposition, :texttemplate, :tickwidth, :totals, :transpose,
+                        :uirevision, :upperfence, :unselected,
+                        :values, :vertexcolor, :visible,
+                        :whiskerwidth, :width,
+                        :x, :x0, :xaxis, :xbingroup, :xbins, :xcalendar, :xgap, :xperiod, :xperiodalignment, :xperiod0, :xtype,
+                        :y, :y0, :yaxis, :ybingroup, :ybins, :ycalendar, :ygap, :yperiod, :yperiodalignment, :yperiod0, :ytype,
+                        :z, :zauto, :zcalendar, :zhoverformat, :zmax, :zmid, :zmin, :zsmooth])
 end
 
 function optionals!(d::Dict, pd::PlotData, opts::Vector{Symbol}) :: Dict
