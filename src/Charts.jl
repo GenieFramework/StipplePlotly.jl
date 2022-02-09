cccea77cbb818f6b141e2a6744133a0d987fa4a4
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-02-09 20:34:40 +0530

    x and y in PlotData can take Matrix values
    Regarding this issue: https://github.com/GenieFramework/StipplePlotly.jl/issues/30

--- src/Charts.jl
+++ src/Charts.jl
@@ -1055,7 +1055,7 @@ Base.@kwdef mutable struct PlotData
   w::Union{Vector,Nothing} = nothing
   whiskerwidth::Union{Float64,Nothing} = nothing
   width::Union{Int,Vector{Int},Nothing} = nothing
-  x::Union{Vector,Nothing} = nothing
+  x::Union{Vector,Matrix,Nothing} = nothing
   x0::Union{Int,String,Nothing} = nothing
   xaxis::Union{String,Nothing} = nothing
   xbingroup::Union{String,Nothing} = nothing
@@ -1066,7 +1066,7 @@ Base.@kwdef mutable struct PlotData
   xperiodalignment::Union{String,Nothing} = nothing
   xperiod0::Union{Float64,Int,String,Nothing} = nothing
   xtype::Union{String,Nothing} = nothing
-  y::Union{Vector,Nothing} = nothing
+  y::Union{Vector,Matrix,Nothing} = nothing
   y0::Union{Int,String,Nothing} = nothing
   yaxis::Union{String,Nothing} = nothing
   ybingroup::Union{String,Nothing} = nothing
