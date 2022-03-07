23b8d1e89666466b45592703ae371af6a097c4b0
hhaensel <helmut.haensel@merckgroup.com>
2022-03-07 11:33:43 +0100

    fixes for PlotlyBase support

--- src/StipplePlotly.jl
+++ src/StipplePlotly.jl
@@ -1,7 +1,6 @@
 module StipplePlotly
 
 using Genie, Stipple, Stipple.Reexport
-using Requires
 
 #===#
 
@@ -60,29 +59,6 @@ include("Charts.jl")
 
 function __init__()
   Stipple.DEPS[@__MODULE__] = deps
-
-  @require PlotlyBase = "a03496cd-edff-5a9b-9e67-9cda94a718b5" begin
-    function Charts.plot(fieldname::Union{Symbol,AbstractString};
-      layout::Union{Symbol,PlotLayout,AbstractString,PlotlyBase.Layout} = PlotLayout(),
-      config::Union{Symbol,PlotConfig,AbstractString,PlotlyBase.PlotConfig} = StipplePlotly.PlotConfig(),
-      args...) :: String
-
-      k = (Symbol(":data"), Symbol(":layout"), Symbol(":config"))
-      v = Any["$fieldname", layout, config]
-
-      Charts.plotly(; args..., NamedTuple{k}(v)...)
-    end
-    
-    function plotly(p::Symbol; layout = "p.layout", config = "p.config", kwargs...)
-      plot("p.data"; layout, config, kwargs...)
-    end
-
-    Base.print(io::IO, a::Union{PlotlyBase.PlotConfig}) = print(io, Stipple.json(a))
-    StructTypes.StructType(::Type{<:PlotlyBase.HasFields}) = JSON3.RawType()
-    StructTypes.StructType(::Type{PlotlyBase.PlotConfig}) = JSON3.RawType()
-
-    JSON3.rawbytes(x::Union{PlotlyBase.HasFields,PlotlyBase.PlotConfig}) = codeunits(PlotlyBase.JSON.json(x))
-  end
 end
 
 end # module
