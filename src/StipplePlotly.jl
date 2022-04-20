61c710d2744a290f87ba1949d0c5b3819ceeb286
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-04-21 00:34:09 +0200

    New deps loading

--- src/StipplePlotly.jl
+++ src/StipplePlotly.jl
@@ -8,41 +8,43 @@ const assets_config = Genie.Assets.AssetsConfig(package = "StipplePlotly.jl")
 
 #===#
 
-function deps() :: Vector{String}
-  if ! Genie.Assets.external_assets(Stipple.assets_config)
+function deps_routes() :: Nothing
+  Genie.Assets.external_assets(Stipple.assets_config) && return nothing
 
-    Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="plotly2.min")) do
-      Genie.Renderer.WebRenderable(
-        Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="plotly2.min.js")),
-        :javascript) |> Genie.Renderer.respond
-    end
-
-    Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="resizesensor.min")) do
-      Genie.Renderer.WebRenderable(
-        Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="resizesensor.min.js")),
-        :javascript) |> Genie.Renderer.respond
-    end
-
-    Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="lodash.min")) do
-      Genie.Renderer.WebRenderable(
-        Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="lodash.min.js")),
-        :javascript) |> Genie.Renderer.respond
-    end
-
-    Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="vueresize.min")) do
-      Genie.Renderer.WebRenderable(
-        Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="vueresize.min.js")),
-        :javascript) |> Genie.Renderer.respond
-    end
-
-    Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="vueplotly.min")) do
-      Genie.Renderer.WebRenderable(
-        Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="vueplotly.min.js")),
-        :javascript) |> Genie.Renderer.respond
-    end
-
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="plotly2.min"), named = :get_plotly2js) do
+    Genie.Renderer.WebRenderable(
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="plotly2.min.js")),
+      :javascript) |> Genie.Renderer.respond
   end
 
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="resizesensor.min"), named = :get_resizesensorjs) do
+    Genie.Renderer.WebRenderable(
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="resizesensor.min.js")),
+      :javascript) |> Genie.Renderer.respond
+  end
+
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="lodash.min"), named = :get_lodashjs) do
+    Genie.Renderer.WebRenderable(
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="lodash.min.js")),
+      :javascript) |> Genie.Renderer.respond
+  end
+
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="vueresize.min"), named = :get_vueresizejs) do
+    Genie.Renderer.WebRenderable(
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="vueresize.min.js")),
+      :javascript) |> Genie.Renderer.respond
+  end
+
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="vueplotly.min"), named = :get_vueplotlyjs) do
+    Genie.Renderer.WebRenderable(
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="vueplotly.min.js")),
+      :javascript) |> Genie.Renderer.respond
+  end
+
+  nothing
+end
+
+function deps() :: Vector{String}
   [
     Genie.Renderer.Html.script(src=Genie.Assets.asset_path(assets_config, :js, file="plotly2.min")),
     Genie.Renderer.Html.script(src=Genie.Assets.asset_path(assets_config, :js, file="resizesensor.min")),
@@ -58,7 +60,8 @@ include("Charts.jl")
 @reexport using .Charts
 
 function __init__()
-  Stipple.DEPS[@__MODULE__] = deps
+  deps_routes()
+  Stipple.deps!(@__MODULE__, deps)
 end
 
 end # module
