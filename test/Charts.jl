58af3ef629b8c9f709dcffd4c5778b98a0a2276c
Dr. Zygmunt L. Szpak <zygmunt.szpak@gmail.com>
2022-12-13 14:13:24 +1030

    Fixes incorrect handling of tooltip text for scatter plots (#53)
    Co-authored-by: Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>

---
+++ test/Charts.jl
@@ -0,0 +1,37 @@
+function create_example_dataframe()
+    xs = [1.0, 2.0, 3.0, 4.0]
+    ys = [10.0, 20.0, 30.0, 40.0]
+    groups = [1, 2, 1, 2]
+    labels = ["A", "C", "B", "D"]
+    return DataFrame(X = xs, Y = ys, Group = groups, Label = labels)
+end
+
+@testset "Scatter Plots" begin
+    @info "Test: Scatter Plots"
+    @testset "Multiple Groups without Tooltips" begin
+        df = create_example_dataframe()
+        pd = plotdata(df, :X, :Y; groupfeature = :Group)
+        @test length(pd) == 2
+        @test length(pd[1].x) ==  length(pd[2].x)
+        @test length(pd[1].y) ==  length(pd[2].y)
+        @test isnothing(pd[1].text) 
+        @test isnothing(pd[2].text) 
+    end
+    @testset "Multiple Groups with Tooltips" begin
+        df = create_example_dataframe()
+        pd = plotdata(df, :X, :Y; groupfeature = :Group, text = df.Label)
+        @test length(pd) == 2
+        @test length(pd[1].x) ==  length(pd[2].x) == 2
+        @test length(pd[1].y) ==  length(pd[2].y) == 2
+        @test !isnothing(pd[1].text) 
+        @test !isnothing(pd[2].text) 
+        if !isnothing(pd[1].text) && !isnothing(pd[2].text) 
+            @test length(pd[1].text) == length(pd[1].x) == length(pd[1].y) == 2
+            @test length(pd[2].text) == length(pd[2].x) == length(pd[2].x) == 2 
+            @test pd[1].text[1] == "A"
+            @test pd[1].text[2] == "B"
+            @test pd[2].text[1] == "C"
+            @test pd[2].text[2] == "D"
+        end
+    end
+end
