fa19ba665f1a0646a3cd17750ccf2d61039456f7
Helmut Hänsel <helmut.haensel@gmx.de>
2024-05-29 01:42:43 +0200

    stipple_parse methods for parametric types

--- ext/StipplePlotlyPlotlyBaseExt.jl
+++ ext/StipplePlotlyPlotlyBaseExt.jl
@@ -17,7 +17,7 @@ function Base.Dict(p::PlotlyBase.Plot)
 end
 
 function PlotlyBase.Plot(d::AbstractDict)
-    sd = symbol_dict(d)
+    sd = PlotlyBase._symbol_dict(d)
     data = haskey(sd, :data) && ! isempty(sd[:data]) ? PlotlyBase.GenericTrace.(sd[:data]) : PlotlyBase.GenericTrace[]
     layout = haskey(sd, :layout) ? PlotlyBase.Layout(sd[:layout]) : PlotlyBase.Layout()
     frames = haskey(sd, :frames) && ! isempty(sd[:frames]) ? PlotlyBase.PlotlyFrame.(sd[:frames]) : PlotlyBase.PlotlyFrame[]
@@ -30,12 +30,30 @@ function Stipple.stipple_parse(::Type{PlotlyBase.Plot}, d::AbstractDict)
     PlotlyBase.Plot(d)
 end
 
+function Stipple.stipple_parse(::Type{Plot{TT, TL, TF}}, d::AbstractDict) where {TT, TL, TF}
+    Plot{TT, TL, TF}(
+        stipple_parse(TT, get(d, "data", get(d, :data, GenericTrace[]))),
+        stipple_parse(TL, get(d, "layout", get(d, :layout, PlotlyBase.Layout()))),
+        stipple_parse(TF, get(d, "frames", get(d, :frames, PlotlyBase.PlotlyFrame[]))),
+        PlotlyBase.uuid4(),
+        stipple_parse(PlotlyBase.PlotConfig, get(d, "config", get(d, :config, PlotlyBase.PlotConfig())))
+    )
+end
+
 function Stipple.stipple_parse(::Type{T}, d::AbstractDict) where T <: PlotlyBase.AbstractTrace
-    T(d)
+    T === PlotlyBase.AbstractTrace ? GenericTrace(d) : T(d)
+end
+
+function Stipple.stipple_parse(T::Type{PlotlyBase.GenericTrace{D}}, d::AbstractDict) where D <: AbstractDict
+    T(stipple_parse(D, PlotlyBase._symbol_dict(d)))
 end
 
 function Stipple.stipple_parse(::Type{PlotlyBase.Layout}, d::AbstractDict)
     PlotlyBase.Layout(d)
+end
+
+function Stipple.stipple_parse(::Type{PlotlyBase.Layout{D}}, d::AbstractDict) where D
+    PlotlyBase.Layout(stipple_parse(D, PlotlyBase._symbol_dict(d)))
 end
 
 end
