35cc8fa31a0ad5f4e250d8530e1c6832ece9b161
Helmut Hänsel <helmut.haensel@gmx.de>
2026-05-17 13:52:39 +0200

    add docstring on frames

--- src/Charts.jl
+++ src/Charts.jl
@@ -81,18 +81,28 @@ function default_config_type()
 end
 
 """
-    function plotly(p::Symbol; layout = Symbol(p, ".layout"), config = Symbol(p, ".config"), configtype = default_config_type(), keepselection = false, kwargs...)
+    function plotly(p::Symbol; layout = Symbol(p, ".layout"), config = Symbol(p, ".config"), frames = Symbol(p, ".frames"), configtype = default_config_type(), keepselection = false, kwargs...)
 
 This is a convenience function for rendering a PlotlyBase.Plot or a struct with fields data, layout and config
 # Example
 ```julia
 julia> plotly(:plot)
-"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"plot.config\"></plotly>"
+"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"plot.config\" :frames=\"plot.frames\"></plotly>"
 ```
 For multiple plots with a common config or layout a typical usage is
 ```julia
 julia> plotly(:plot, config = :config)
-"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"config\"></plotly>"
+"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"config\" :frames=\"plot.frames\"></plotly>"
+```
+To use animation frames stored separately from the plot object:
+```julia
+julia> plotly(:plot, frames = :myframes)
+"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"plot.config\" :frames=\"myframes\"></plotly>"
+```
+To disable frame binding (e.g. for non-animated plots):
+```julia
+julia> plotly(:plot, frames = nothing)
+"<plotly :data=\"plot.data\" :layout=\"plot.layout\" :config=\"plot.config\"></plotly>"
 ```
 
 """
